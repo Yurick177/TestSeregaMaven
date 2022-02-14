b9ef3bd45fadaeb7abdcd14ade65b89e80a544cf
Ura <Yurick1771081@gmail.com>
2022-02-14 23:20:40 +0300

    new Test

--- src/test/java/TestLocation.java
+++ src/test/java/TestLocation.java
@@ -1,53 +1,59 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 import java.time.Duration;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class TestLocation {
 
     public WebDriver driver;
     public WebDriverWait wait;
 
-    @BeforeTest
+    @BeforeEach
     public void before() {
-        System.setProperty("webdriver.chrome.driver", "C:\\FAMILY\\Yura\\webDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     @Test
     public void testPositive() {
-        try {
-            driver.get(Util.URL);
-            driver.findElement(By.id("username")).sendKeys(Util.LOGIN_NAME_POSITIVE);
-            driver.findElement(By.id("password")).sendKeys(Util.LOGIN_PASSWORD_POSITIVE);
-            driver.findElement(By.xpath("//i[text()=\" Login\"]")).submit();
-            wait.until(presenceOfElementLocated(By.xpath("//i[@class='icon-2x icon-signout']"))).click();
-        } catch (Exception e) {
-            System.out.println("ошибка");
-        } finally {
-            driver.quit();
-        }
+        driver.get(Util.URL);
+        driver.findElement(By.id("username")).sendKeys(Util.LOGIN_NAME_POSITIVE);
+        driver.findElement(By.id("password")).sendKeys(Util.LOGIN_PASSWORD_POSITIVE);
+        driver.findElement(By.xpath("//i[text()=' Login']")).submit();
+        String actText = driver.findElement(By.xpath("//h4[@class='subheader']")).getText();
+        String expeText = "Welcome to the Secure Area. When you are done click logout below.";
+        Assertions.assertEquals(expeText, actText);
+
     }
 
     @Test
     public void testNegative() {
-        try {
-            driver.get(Util.URL);
-            driver.findElement(By.id("username")).sendKeys(Util.LOGIN_NAME_NEGATIVE);
-            driver.findElement(By.id("password")).sendKeys(Util.LOGIN_PASSWORD_NEGATIVE);
-            driver.findElement(By.xpath("//i[text()=\" Login\"]")).submit();
-        } catch (Exception e) {
-            System.out.println("ошибка");
-        } finally {
-            driver.quit();
-        }
+        driver.get(Util.URL);
+        driver.findElement(By.id("username")).sendKeys(Util.LOGIN_NAME_NEGATIVE);
+        driver.findElement(By.id("password")).sendKeys(Util.LOGIN_PASSWORD_NEGATIVE);
+        driver.findElement(By.xpath("//i[text()=' Login']")).submit();
+        String actualText = driver.findElement(By.xpath("//h4[@class='subheader']")).getText();
+        String expectedText = "This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.";
+        Assertions.assertEquals(expectedText,actualText);
+
     }
+    @Test
+    public void testLogOut(){
+        driver.get(Util.URL);
+        driver.findElement(By.id("username")).sendKeys(Util.LOGIN_NAME_POSITIVE);
+        driver.findElement(By.id("password")).sendKeys(Util.LOGIN_PASSWORD_POSITIVE);
+        driver.findElement(By.xpath("//i[text()=' Login']")).submit();
+        driver.findElement(By.xpath("//i[@class='icon-2x icon-signout']")).click();
+        String actualText = driver.findElement(By.xpath("//h4[@class='subheader']")).getText();
+        String expectedText = "This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.";
+        Assertions.assertEquals(expectedText,actualText);
+    }
+
 
 }
--- src/test/java/Util.java
+++ src/test/java/Util.java
@@ -6,5 +6,10 @@ public class Util {
     public static final String LOGIN_PASSWORD_POSITIVE = "SuperSecretPassword!";
     public static final String LOGIN_NAME_NEGATIVE = "Yuriy";
     public static final String LOGIN_PASSWORD_NEGATIVE = "123!";
+    public static final String A = "//h4[@class='subheader']";
+    public static final String B = "Welcome to the Secure Area. When you are done click logout below.";
+    public static final String C = "//h4[text()='Welcome to the Secure Area. When you are done click logout below.']";
+
+
 
 }
