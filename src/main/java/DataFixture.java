45204a112578d1a400db0d0de2f73beca8657039
Ura <Yurick1771081@gmail.com>
2022-04-04 02:07:58 +0300

    add annotation @FindBy and dependency(WebDriverManager)

--- src/main/java/DataFixture.java
+++ src/main/java/DataFixture.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class DataFixture {
     protected static WebDriver driver;
@@ -26,11 +28,16 @@ public class DataFixture {
 
     @BeforeEach
     public void beforeTest() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+//        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+//        driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
     }
 
     @AfterEach
     public void afterTest() {
+        driver.close();
         driver.quit();
     }
 
--- src/main/java/pageObject/LoginPage.java
+++ src/main/java/pageObject/LoginPage.java
@@ -1,38 +1,53 @@
 package pageObject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-    private final By usernameField = By.id("username");
-    private final By passwordField = By.id("password");
-    private final By signInButton = By.cssSelector(".radius");
-    private final By logOutButton = By.className("button");
-    private final By loginPageText = By.className("subheader");
-    private final By logOutPageText = By.className("subheader");
-    private final WebDriver driver;
+    @FindBy(id = "username")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(css = ".radius")
+    private WebElement signInButton;
+
+    @FindBy(className = "button")
+    private WebElement logOutButton;
+
+    @FindBy(className = "subheader")
+    private WebElement loginPageText;
+
+    @FindBy(className = "subheader")
+    private WebElement logOutPageText;
+
+    private WebDriver driver;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void authorization(String name, String password) {
-        driver.findElement(usernameField).sendKeys(name);
-        driver.findElement(passwordField).sendKeys(password);
-        driver.findElement(signInButton).submit();
+        usernameField.sendKeys(name);
+        passwordField.sendKeys(password);
+        signInButton.submit();
     }
 
     public void clickLogOut() {
-        driver.findElement(logOutButton).click();
+        logOutButton.click();
     }
 
     public String getLoginPageText() {
-        return driver.findElement(loginPageText).getText();
+        return loginPageText.getText();
     }
 
-    public String getLogOutPageText(){
-        return driver.findElement(logOutPageText).getText();
+    public String getLogOutPageText() {
+        return logOutPageText.getText();
     }
 
 }
